25b1da3ea084cf313b39e90ff78368d86947094c
Finn <SS_ThinkOf@hotmail.com>
2016-03-30 23:19:07 +0800

    Mission-26

--- Mission-26/src/js/space.js
+++ Mission-26/src/js/space.js
@@ -1,41 +1,35 @@
+
 /**
  * 时钟类
  */
 
 function Timer(interval) {
     this.interval = interval;
-    this.listener = [];
+    this.listener = new Set();
     this.lastDate = new Date();
     setInterval(this.tita.bind(this), interval);
 }
 
 Timer.prototype.tita = function () {
+    var listeners = this.listener.array();
     //所有注册时钟
     var now = new Date();
-    for (var i = 0, len = this.listener.length; i < len; i++) {
-        setTimeout(function (listener,speed) {return function () {listener(speed)}}(this.listener[i],now - this.lastDate), 0);
+    for (var i = 0, len = listeners.length; i < len; i++) {
+        setTimeout(function (listener, speed) {
+            return function () {
+                listener(speed)
+            }
+        }(listeners[i], now - this.lastDate), 0);
     }
     this.lastDate = now;
 }
 
 Timer.prototype.addListener = function (listener) {
-    var exist = false;
-    for (var i = 0, len = this.listener.length; i < len; i++) {
-        if (this.listener[i] == listener) {
-            exist = true;
-        }
-    }
-    if (!exist) {
-        this.listener.push(listener);
-    }
+    this.listener.put(listener);
 }
 
 Timer.prototype.removeListener = function (listener) {
-    for (var i = 0, len = this.listener.length; i < len; i++) {
-        if (this.listener == listener) {
-            this.listener.splice(i, 1);
-        }
-    }
+    this.listener.remove(listener);
 }
 
 Timer.prototype.timeout = function (callback, time) {
@@ -65,13 +59,13 @@ function Space() {
     }
 
     //宇宙中的物体
-    this.item = {}
+    this.item = new Set();
 
     //宇宙中的无线电接收器
-    this.radios = {};
+    this.radios = new Set();
 
     //宇宙中的能源接收器（太阳能电板）
-    this.energyReceiver = {};
+    this.energyReceiver = new Set();
 
 
     //时钟
@@ -83,11 +77,11 @@ function Space() {
 }
 
 Space.prototype.addRadio = function (radio) {
-    this.radios[radio.id] = radio;
+    this.radios.put(radio);
 }
 
 Space.prototype.removeRadio = function (radio) {
-    delete this.radios[radio.id];
+    this.radios.remove(radio);
 }
 
 Space.prototype.broadcastMessage = function (packet) {
@@ -100,15 +94,15 @@ Space.prototype.broadcastMessage = function (packet) {
             console.log("消息已丢包");
         }
     }
-}
+};
 
 Space.prototype.addEnergyReceiver = function (receiver) {
-    this.energyReceiver[receiver.id] = receiver;
-}
+    this.energyReceiver.put(receiver);
+};
 
 Space.prototype.removeEnergyReceiver = function (receiver) {
-    delete this.energyReceiver[receiver.id];
-}
+    this.energyReceiver.remove(receiver);
+};
 
 //收到能量
 Space.prototype.receiveEnergy = function (power) {
@@ -123,4 +117,11 @@ Space.prototype.radiantEnergy = function (power) {
     }
 };
 
-//增加
+//增加宇宙中的物体
+Space.prototype.addItem = function (item) {
+    this.item.put(item);
+}
+
+Space.prototype.removeItem = function (item) {
+    this.item.remove(item);
+}
